41199e4787101166fafe5ecc2b733367e7d0f333
bqprojects <support@brainquest.info>
2025-09-22 10:31:10 +0100

    ghhgc

--- wandlen_temp/src/routing/router.jsx
+++ wandlen_temp/src/routing/router.jsx
@@ -12,7 +12,7 @@ import Aran from "../pages/website/Aran";
 import BecomeVolunteer from "../pages/website/BecomeVolunteer";
 import CameraTips from "../pages/website/CameraTips";
 import ChooseYourExperience from "../pages/website/ChoseYourExperience";
-import HowItWorks from "../pages/volunteer/HowitWork.jsx";
+import HowItWorks from "../pages/volunteer/HowitWork";
 import NatureWalking from "../pages/website/NatureWalking";
 import OurApproach from "../pages/website/OurApproach";
 import PaymentPageForIndividual from "../pages/website/PaymentPageForIndividual";
